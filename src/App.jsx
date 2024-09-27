6dca142616f6e5e09ea213f9e2bc3e31f88af3ce
Yogesh Singh <yogeshsinghbhati03@gmail.com>
2024-09-27 16:49:04 +0530

    FInal commit

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import WeAreGlobal from "./Components/WeAreGlobal";
 
 function App() {
   return (
-    <div className="mx-0 lg:mx-24 ">
+    <div className="mx-0 lg:mx-24 overflow-hidden">
       <Navbar />
       <Body />
       <LogoSlider />
